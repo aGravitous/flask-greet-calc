b4716821277de97d4d85a5c645a249e24051bb75
meowcodes <meowcodes@gmail.com>
2019-02-13 12:34:44 -0800

    added all routes/functions to calc

--- calc/app.py
+++ calc/app.py
@@ -2,10 +2,31 @@ from flask import Flask, request
 import operations
 app = Flask(__name__)
 
-
 @app.route("/<operation>")
 def operate(operation):
     a = int(request.args["a"])
     b = int(request.args["b"])
+
     if operation == "add":
         return str(operations.add(a, b))
+    elif operation == "sub":
+        return str(operations.sub(a, b))
+    elif operation == "mult":
+        return str(operations.mult(a, b))
+    elif operation == "div":
+        return str(operations.div(a, b))
+
+
+@app.route("/math/<operation>")
+def operate_math(operation):
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    if operation == "add":
+        return str(operations.add(a, b))
+    elif operation == "sub":
+        return str(operations.sub(a, b))
+    elif operation == "mult":
+        return str(operations.mult(a, b))
+    elif operation == "div":
+        return str(operations.div(a, b))
