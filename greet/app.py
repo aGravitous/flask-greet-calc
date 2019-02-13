04664d8eb6c79e526ee1cc894df24f9466bdc0e3
meowcodes <meowcodes@gmail.com>
2019-02-13 12:05:48 -0800

    added routes for welcome/<text>

--- greet/app.py
+++ greet/app.py
@@ -5,12 +5,55 @@ app = Flask(__name__)
 def welcome_page():
     """Greets visitors."""
 
-    html = """
+    html = f"""
     <html>
         <head></head>
         <body>
-            <h1>Welcome!</h1>
+            <h1>Welcome</h1>
         </body>
     </html>
     """
     return html
+
+
+@app.route('/welcome/<text>')
+def welcome_page_with_text(text=""):
+    """Greets visitors."""
+
+    html = f"""
+    <html>
+        <head></head>
+        <body>
+            <h1>Welcome {text}</h1>
+        </body>
+    </html>
+    """
+    return html
+
+# @app.route('/welcome/home')
+# def welcome_page():
+#     """Greets visitors."""
+
+#     html = """
+#     <html>
+#         <head></head>
+#         <body>
+#             <h1>Welcome home</h1>
+#         </body>
+#     </html>
+#     """
+#     return html
+
+# @app.route('/welcome/back')
+# def welcome_page():
+#     """Greets visitors."""
+
+#     html = """
+#     <html>
+#         <head></head>
+#         <body>
+#             <h1>Welcome back</h1>
+#         </body>
+#     </html>
+#     """
+#     return html
