03c0fe71b8fe45f6027fca982ab80ba35d89d49d
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-13 11:55:19 -0800

    Added welcome page.

--- greet/app.py
+++ greet/app.py
@@ -1,2 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome_page():
+    """Greets visitors."""
+
+    html = """
+    <html>
+        <head></head>
+        <body>
+            <h1>Welcome!</h1>
+        </body>
+    </html>
+    """
+    return html
