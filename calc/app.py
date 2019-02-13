59625c8a0eec44fc49196bd8c57d55ea5f921c34
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-13 12:29:18 -0800

    Added add route and functionality in calc.

--- calc/app.py
+++ calc/app.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+import operations
 app = Flask(__name__)
 
+
+@app.route("/<operation>")
+def operate(operation):
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    if operation == "add":
+        return str(operations.add(a, b))
